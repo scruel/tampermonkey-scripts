fd792e54c892abd4b5b282ad6e2f396dbe3f42d8
Scruel Tao <scruelt@hotmail.com>
2023-03-07 11:06:38 +0800

    Update chatgpt-latex-render.js
    Use MutationObserver mentioned in https://github.com/scruel/tampermonkey-scripts/issues/3, thanks @duanzheng

--- chatgpt-latex-render.js
+++ chatgpt-latex-render.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name               ChatGPT LaTeX Auto Render (OpenAI, you, bing, etc.)
+// @name               ChatGPT LaTeX Auto Render (OpenAI, you, new bing, etc.)
 // @namespace          http://tampermonkey.net/
-// @version            0.4.5
-// @author             Scruel
+// @version            0.5.0
+// @author             Scruel Tao
 // @homepage           https://github.com/scruel/tampermonkey-scripts
 // @description        Auto typeset LaTeX math formulas on ChatGPT pages (OpenAI, new bing, you, etc.).
 // @description:zh-CN  自动渲染 ChatGPT 页面 (OpenAI, new bing, you 等) 上的 LaTeX 数学公式。
@@ -10,94 +10,130 @@
 // @match              https://you.com/search?*&tbm=youchat*
 // @match              https://www.bing.com/search?*
 // @grant              none
+// @noframes
 // ==/UserScript==
 
 'use strict';
 
 const _parsed_mark = '_sc_parsed';
-
 function queryAddNoParsed(query) {
     return query + ":not([" + _parsed_mark + "])";
 }
 
-function prepareScript() {
-    window._sc_afterTypesetElement = (element) => {};
+async function prepareScript() {
+    window._sc_typesetAfter = (element) => {};
     window._sc_typeset = () => {
-        if (window._sc_typesetting) {
-            return;
-        }
-        window._sc_typesetting = true;
         try {
             const messages = window._sc_getMessages();
-            messages.forEach((element) => {
-                element.setAttribute(_parsed_mark,'');
-                MathJax.typesetPromise([element]);
-                window._sc_afterTypesetElement(element);
+            messages.forEach(msg => {
+                // Prevent conflict latex typeset (katex).
+                const katexEles = msg.querySelectorAll('.katex-display');
+                katexEles.forEach(e => {
+                    const mlEle = e.querySelector(".katex-mathml");
+                    e.parentElement.removeAttribute("class");
+                    e.parentElement.innerHTML = mlEle.innerHTML;
+                });
+                // Typeset latex.
+                msg.setAttribute(_parsed_mark,'');
+                MathJax.typesetPromise([msg]);
+                window._sc_typesetAfter(msg);
             });
-        } catch (ignore) {
+        } catch (e) {
+            console.warn(e);
         }
-        window._sc_typesetting = false;
     }
+    window._sc_mutationHandler = (mutation) => {
+        if (mutation.oldValue === '') {
+            window._sc_typeset();
+        }
+    };
+    window._sc_chatLoaded = () => { return true; };
 
-    const commonCheck = (query) => {
-        const submitButton = document.querySelector(query);
-        return submitButton && !submitButton.disabled;
-    }
+    var afterMainOvservationStart = () => {window._sc_typeset();};
 
     if (window.location.host == "www.bing.com") {
+        window._sc_getObserveElement = () => {
+            const ele = document.querySelector("#b_sydConvCont > cib-serp");
+            if (!ele) {return null;}
+            return ele.shadowRoot.querySelector("#cib-action-bar-main");
+        }
         window._sc_getMessages = () => {
-            const elements = [];
-            const allChatTurn = document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelectorAll("#cib-chat-main > cib-chat-turn");
-            if (allChatTurn.length == 0) {
-                return;
+            try {
+                const allChatTurn = document.querySelector("#b_sydConvCont > cib-serp").shadowRoot.querySelector("#cib-conversation-main").shadowRoot.querySelectorAll("#cib-chat-main > cib-chat-turn");
+                const allCibMsgGroup = allChatTurn[allChatTurn.length - 1].shadowRoot.querySelectorAll("cib-message-group");
+                const allCibMsg = Array.from(allCibMsgGroup).map(e => Array.from(e.shadowRoot.querySelectorAll("cib-message"))).flatMap(e => e);
+                return Array.from(allCibMsg).map(cibMsg => cibMsg.shadowRoot.querySelector("cib-shared")).filter(e => e);
+            } catch (ignore) {
+                return [];
             }
-            const allCibMeg = allChatTurn[allChatTurn.length - 1].shadowRoot.querySelector("cib-message-group.response-message-group").shadowRoot.querySelectorAll("cib-message");
-            allCibMeg.forEach((cibMeg) => {
-                const element = cibMeg.shadowRoot.querySelector(queryAddNoParsed("cib-shared"));
-                if (element) {
-                    elements.push(element);
-                }
-            });
-            return elements;
-        }
-        window._sc_isAnswerPrepared = () => {
-            const actionBarParent = document.querySelector("#b_sydConvCont > cib-serp");
-            if (!actionBarParent) {
-                return false;
-            }
-            const actionBar = actionBarParent.shadowRoot.querySelector("#cib-action-bar-main");
-            return actionBar && !actionBar.hasAttribute('cancelable');
-        }
-    }
-    else if (window.location.host == "you.com") {
-        window._sc_getMessages = () => {
-            return document.querySelectorAll(queryAddNoParsed('#chatHistory div[data-testid="youchat-answer"]'));
-        }
-        window._sc_isAnswerPrepared = () => {
-            return commonCheck('main div[data-testid="youchat-input"] textarea+button');
         }
     }
     else if (window.location.host == "chat.openai.com") {
         window._sc_getMessages = () => {
             return document.querySelectorAll(queryAddNoParsed("div.w-full div.text-base div.items-start"));
         }
-        window._sc_afterTypesetElement = (element) => { element.style.display = 'unset';}
-        window._sc_isAnswerPrepared = () => {
-            return commonCheck('main form textarea+button');
+        window._sc_getObserveElement = () => {
+            return document.querySelector("main form textarea+button");
+        }
+        window._sc_typesetAfter = (element) => { element.style.display = 'unset';}
+        window._sc_chatLoaded = () => { return document.querySelector('main div.text-sm>svg.animate-spin') === null; };
+
+        afterMainOvservationStart = () => {
+            window._sc_typeset();
+            // Handle conversation switch
+            new MutationObserver((mutationList) => {
+                mutationList.forEach(async (mutation) => {
+                    if (mutation.addedNodes){
+                        window._sc_typeset();
+                        startMainOvservation(await getMainObserveElement(true));
+                    }
+                });
+            }).observe(document.querySelector('#__next'), {childList: true});
+        };
+    }
+    else if (window.location.host == "you.com") {
+        window._sc_getMessages = () => {
+            return document.querySelectorAll(queryAddNoParsed('#chatHistory div[data-testid="youchat-answer"]'));
+        }
+        window._sc_getObserveElement = () => {
+            return document.querySelector('main div[data-testid="youchat-input"] textarea+button');
         }
     }
+    console.log('Waiting for chat loading...')
+    const mainElement = await getMainObserveElement();
+    console.log('Chat loaded.')
+    startMainOvservation(mainElement);
+    afterMainOvservationStart();
 }
 
-function renderTrigger() {
-    setTimeout(renderLatex, window.renderDelay);
+function getMainObserveElement(chatLoaded=false) {
+  return new Promise(async (resolve, reject) => {
+      const resolver = () => {
+          const ele = window._sc_getObserveElement();
+          if (ele && (chatLoaded || window._sc_chatLoaded())) {
+              return resolve(ele);
+          }
+          window.setTimeout(resolver, 500);
+      }
+      resolver();
+  });
 }
 
-function renderLatex() {
-    if (window._sc_isAnswerPrepared()) {
-        // console.log("Rendering...")
-        window._sc_typeset();
+async function startMainOvservation(mainElement) {
+    const observerOptions = {
+        attributeOldValue : true,
+        attributeFilter: ['cancelable', 'disabled'],
+    };
+    const callback = (mutationList, observer) => {
+        mutationList.forEach(mutation => {
+            window._sc_mutationHandler(mutation);
+        });
+    };
+    if (window._sc_mainObserver) {
+        window._sc_mainObserver.disconnect();
     }
-    renderTrigger();
+    window._sc_mainObserver = new MutationObserver(callback);
+    window._sc_mainObserver.observe(mainElement, observerOptions);
 }
 
 async function addScript(url) {
@@ -108,31 +144,16 @@ async function addScript(url) {
         headElement.append(scriptElement);
     }
     scriptElement.src = url;
-
-    await waitScriptLoaded();
 }
 
-function waitScriptLoaded() {
-  return new Promise(async (resolve, reject) => {
-      const resolver = () => {
-          if (MathJax.hasOwnProperty('typeset')) {
-              resolve();
-              return;
-          }
-          if (window._sc_ChatLatex.loadCount > 100) {
-              setTipsElementText("Failed to load MathJax, try refresh.");
-              reject("Failed to load MathJax, try refresh.");
-              return;
-          }
-          window._sc_ChatLatex.loadCount += 1;
-          window.setTimeout(resolver, 200);
-      }
-      resolver();
-  });
-}
-
-function setTipsElementText(text) {
-    window._sc_ChatLatex.tipsElement.innerHTML = text;
+async function waitMathJaxLoaded() {
+    while (!MathJax.hasOwnProperty('typeset')) {
+        if (window._sc_ChatLatex.loadCount > 20000 / 200) {
+            setTipsElementText("Failed to load MathJax, try refresh.", true);
+        }
+        await new Promise((x) => setTimeout(x, 500));
+        window._sc_ChatLatex.loadCount += 1;
+    }
 }
 
 function showTipsElement() {
@@ -145,29 +166,39 @@ function showTipsElement() {
     document.body.appendChild(tipsElement);
 }
 
+function setTipsElementText(text, errorRaise=false) {
+    window._sc_ChatLatex.tipsElement.innerHTML = text;
+    if (errorRaise) {
+        throw text;
+    }
+    console.log(text);
+}
+
 function hideTipsElement(timeout=3) {
-    window.setTimeout(() => {window._sc_ChatLatex.tipsElement.hidden=true}, 3000);
+    window.setTimeout(() => {window._sc_ChatLatex.tipsElement.hidden=true; }, 3000);
+}
+
+async function loadMathJax() {
+    showTipsElement();
+    setTipsElementText("Loading MathJax...");
+    addScript('https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js');
+    await waitMathJaxLoaded();
+    setTipsElementText("MathJax Loaded.");
+    hideTipsElement();
 }
 
 (async function() {
     window._sc_ChatLatex = {
         tipsElement: document.createElement("div"),
         loadCount: 0
-    }
+    };
     window.MathJax = {
         tex: {
             inlineMath: [['$', '$'], ['\\(', '\\)']],
-            displayMath  : [['$$', '$$']]
+            displayMath  : [['$$', '$$', ['\\[', '\\]']]]
         }
     };
 
-    showTipsElement();
-    setTipsElementText("Loading MathJax...");
-    await addScript('https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js');
-    setTipsElementText("MathJax Loaded.");
-    hideTipsElement();
-
-    prepareScript();
-    window.renderDelay = 1000;
-    renderTrigger();
+     await loadMathJax();
+     await prepareScript();
 })();
